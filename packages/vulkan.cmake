97a6d35d3ef2832fd9a04a72f8e97904fb50d2c6
x <x@y>
2026-09-08 19:37:39 +0200

    mpv: enable libcurl; vulkan: disable static loader (folded from the build repo's patch layer)

--- packages/vulkan.cmake
+++ packages/vulkan.cmake
@@ -4,9 +4,7 @@ ExternalProject_Add(vulkan
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    GIT_TAG e069cc27e242393c46af31898aec633a18e94c91  # FlowVid pin 2026-09-08: revision as of 2026-06-25 (last proven build)
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-*.patch
-    CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+    GIT_TAG e069cc27e242393c46af31898aec633a18e94c91  # FlowVid pin 2026-09-08: revision as of 2026-06-25 (last proven build)    CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
@@ -16,13 +14,7 @@ ExternalProject_Add(vulkan
         -DVULKAN_HEADERS_INSTALL_DIR=${MINGW_INSTALL_PREFIX}
         -DBUILD_TESTS=OFF
         -DENABLE_WERROR=OFF
-        ${vulkan_asm}
-        -DBUILD_STATIC_LOADER=ON
-        -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -D__STDC_FORMAT_MACROS -DSTRSAFE_NO_DEPRECATE -Dparse_number=cjson_parse_number'
-        -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -D__STDC_FORMAT_MACROS -fpermissive'
-    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/loader/libvulkan.a ${MINGW_INSTALL_PREFIX}/lib/libvulkan.a
-            COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/loader/vulkan_own.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/vulkan.pc
+        ${vulkan_asm}    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
